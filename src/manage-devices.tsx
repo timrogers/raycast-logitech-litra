10b3efd100719d6bda9a15691325b8faf2f81579
Tim Rogers <me@timrogers.co.uk>
2023-03-04 14:19:49 +0000

    Allow managing Litra devices' temperature and brightness using presets

--- src/manage-devices.tsx
+++ src/manage-devices.tsx
@@ -1,8 +1,9 @@
 import { ActionPanel, Action, Icon, List, showToast, Toast } from "@raycast/api";
 import React, { useEffect, useState } from "react";
-
 import { getCliDirectory } from "./preferences";
-import { getDevices, turnOff, turnOn } from "./utils";
+import { getDevices, turnOff, turnOn, setTemperatureInKelvin, setBrightnessPercentage } from "./utils";
+import { getEnabledTemperaturePresets } from "./temperature-presets";
+import { getEnabledBrightnessPresets } from "./brightness-presets";
 
 interface Device {
   name: string;
@@ -11,6 +12,8 @@ interface Device {
 
 export default function Command() {
   const [devices, setDevices] = useState<Device[]>([]);
+  const [enabledTemperaturePresets, setEnabledTemperaturePresets] = useState<Set<number>>(new Set());
+  const [enabledBrightnessPresets, setEnabledBrightnessPresets] = useState<Set<number>>(new Set());
 
   const cliDirectory = getCliDirectory();
 
@@ -20,6 +23,22 @@ export default function Command() {
       setDevices(devices);
     })();
   }, []);
+
+  useEffect(() => {
+    (async () => {
+      const enabledTemperaturePresets = await getEnabledTemperaturePresets();
+      setEnabledTemperaturePresets(enabledTemperaturePresets);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const enabledBrightnessPresets = await getEnabledBrightnessPresets();
+      setEnabledBrightnessPresets(enabledBrightnessPresets);
+    })();
+  }, []);
+
+  console.log({ enabledTemperaturePresets, enabledBrightnessPresets });
 
   return (
     <List isLoading={false}>
@@ -46,6 +65,34 @@ export default function Command() {
                   await showToast({ title: `Turned off ${device.name}`, style: Toast.Style.Success });
                 }}
               />
+              {Array.from(enabledTemperaturePresets).map((temperature) => (
+                <Action
+                  key={temperature}
+                  title={`Set Temperature to ${temperature}K`}
+                  icon={Icon.Temperature}
+                  onAction={async () => {
+                    await setTemperatureInKelvin(cliDirectory, device.serial_number, temperature);
+                    await showToast({
+                      title: `Set ${device.name}'s temperature to ${temperature}K`,
+                      style: Toast.Style.Success,
+                    });
+                  }}
+                />
+              ))}
+              {Array.from(enabledBrightnessPresets).map((brightness) => (
+                <Action
+                  key={brightness}
+                  title={`Set Brightness to ${brightness}%`}
+                  icon={Icon.CircleProgress100}
+                  onAction={async () => {
+                    await setBrightnessPercentage(cliDirectory, device.serial_number, brightness);
+                    await showToast({
+                      title: `Set ${device.name}'s brightness to ${brightness}%`,
+                      style: Toast.Style.Success,
+                    });
+                  }}
+                />
+              ))}
             </ActionPanel>
           }
         />
